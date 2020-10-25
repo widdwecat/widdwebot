ba7f59ee62c0e3529cc3645838fa8db03839982c
widdwecat <68664271+widdwecat@users.noreply.github.com>
2020-10-25 13:23:55 -0400

    add bot main file

---
+++ index.js
@@ -0,0 +1,34 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+    host: 'localhost',
+    port: 0,
+    username: 'widdwebot',
+    // password: 'password'
+})
+
+function faceNearestPlayer() {
+    const playerFilter = (entity) => entity.type === 'player'
+    const playerEntity = bot.nearestEntity(playerFilter)
+
+    if(!playerEntity) return
+
+    const pos = playerEntity.position.offset(0, playerEntity.height, 0)
+    bot.lookAt(pos)
+}
+
+bot.on('physicTick', faceNearestPlayer)
+
+bot.on('chat', (username, message) => {
+    if(message === '!help') {
+        bot.chat("Help: https://github.com/widdwecat/widdwebot/blob/main/README.md")
+    }
+
+    if(message === 'widdwebot') {
+        bot.chat("what")
+    }
+
+    if(message === '!coords') {
+        bot.chat("My coords are: " + bot.position)
+    }
+})
